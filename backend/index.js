7f9b65bfac612d81e5c6162201041c94b2062b72
Utkarsh Srivastava <utkarsh.20243303@mnnit.ac.in>
2026-06-28 12:04:23 +0530

    backend almost done

--- backend/index.js
+++ backend/index.js
@@ -3,12 +3,22 @@ import dotenv from 'dotenv'
 import { connectDB } from './config/database.js'
 import userRoute from "./routes/userRoute.js"
 import messageRoute from "./routes/messageRoute.js"
+import cors from "cors"
 import cookieParser from 'cookie-parser'
+import { app, server } from './sockets/socket.js'
 dotenv.config() //loads variables from .env file to process.env
 //where process.env is an object of NODE JS
 
-const app = express()
+// const app = express() // commented as now we have websockets
+
+// Allows your React frontend to talk to this backend
+// Allows the browser to pass cookies/sessions back and forth
+app.use(cors({origin: 'http://localhost:3000', 
+  credentials: true        
+}));
+//Middlewares
 app.use(express.json()); // middleware helps decipher the json data coming 
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
 
 const PORT = process.env.PORT || 8000
@@ -16,7 +26,7 @@ const PORT = process.env.PORT || 8000
 app.use("/api/v1/user", userRoute) // named it userRoute for defining actions 
 app.use("/api/v1/message", messageRoute)
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     connectDB();
     console.log(`Server is listening at port ${PORT}`)
 })
